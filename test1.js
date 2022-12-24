adb1be4ebf63cd049e034a9e693fd862b47e5e58
chen <108784739+chen00000000000@users.noreply.github.com>
2022-12-25 02:20:10 +0800

    Add files via upload

---
+++ test1.js
@@ -0,0 +1,11 @@
+// var banners = ["1.jpg","2.jpg"];
+// var counter =0;
+// function run(){
+//     setInterval(cycle,1000);
+// }
+// function cycle(){
+//     counter++;
+//     if(counter == banners.length)
+//     counter=0;
+//     document.getElementById("banner").src = banners[counter];
+// }
